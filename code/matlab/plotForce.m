ce3a48f408591c876af7744c4e9020e530ebad3e
Griswald Brooks <griswald.brooks@gmail.com>
2015-06-05 21:50:09 -0400

    Converted to official Thesis format

--- code/matlab/plotForce.m
+++
@@ -1,3 +0,0 @@
-function plotForce(occForce, r_pose, plot_scaler, color)
-fr1 = [cos(r_pose(3)),-sin(r_pose(3));sin(r_pose(3)),cos(r_pose(3))]*occForce;
-line([r_pose(1), plot_scaler*fr1(1) + r_pose(1)],[r_pose(2), plot_scaler*fr1(2) + r_pose(2)], 'Color',color);
